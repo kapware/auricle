baa0972f8d92e2bd6d45707b480af91855d727b7
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2017-08-01 13:58:08 +0200

    Renamed button: "Write to file &share" → "Export"

--- src/auricle/ui.cljs
+++ src/auricle/ui.cljs
@@ -94,7 +94,7 @@
 (defn a-share-button [text-on on-press-func speakers]
   [touchable-highlight {:on-press #(on-press-func speakers)}
    [text {:style {:padding 10 :background-color "#999999" :margin-bottom 10}} text-on]])
-(defn write-to-file-and-share-button [speakers] (a-share-button "Write to file &share" write-to-file-and-share speakers))
+(defn write-to-file-and-share-button [speakers] (a-share-button "Export" write-to-file-and-share speakers))
 
 (defn new-speaker []
   (let [speakers (subscribe [:speakers])]
